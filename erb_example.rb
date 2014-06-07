0e2a5c193126951b7fa6e9d185737048e44627d5
wubwubwubwub <nolan.camp@gmail.com>
2014-06-07 15:16:32 -0400

    worked on example 1

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,14 @@
 require 'erb'
-
+y = "HELLO"
+z = 8
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+
+template = ERB.new("The value of x is: <%= x %>")
+simple = ERB.new("if you add the two variables together you get <%= z + x %>")
+yet_again = ERB.new("Can't use inline variables with this method... but <%= y %>!")
+
 puts template.result(binding)
+puts "---------"
+puts simple.result(binding)
+puts "---------"
+puts yet_again.result(binding)
